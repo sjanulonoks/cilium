f5bef39f57508b354a9e78e24e3b87b54ef6389e
Thomas Graf <thomas@cilium.io>
2017-05-13 15:16:36 +0200

    tests: Adjust tests to new policy
    Signed-off-by: Thomas Graf <thomas@cilium.io>

--- tests/10-proxy.sh
+++ tests/10-proxy.sh
@@ -3,6 +3,7 @@
 source "./helpers.bash"
 
 function cleanup {
+	cilium policy delete --all 2> /dev/null || true
 	docker rm -f server client 2> /dev/null || true
 }
 
@@ -36,23 +37,33 @@ set -x
 
 cilium endpoint list
 
+cilium policy delete --all
 cat <<EOF | cilium -D policy import -
-{
-        "name": "root",
-	"rules": [{
-		"coverage": ["id.client"],
-		"l4": [{
-			"out-ports": [{
-				"port": 80, "protocol": "tcp",
-				"l7-parser": "http",
-				"l7-rules": [{ "expr": "Method(\"GET\") && Path(\"/public\")" }]
-			}]
-		}]
-	},{
-		"coverage": ["id.server"],
-		"allow": ["id.client", "reserved:host"]
+[{
+    "endpointSelector": ["id.server"],
+    "ingress": [{
+        "fromEndpoints": [
+	    ["reserved:host"], ["id.client"]
+	]
+    }]
+},{
+    "endpointSelector": ["id.client"],
+    "egress": [{
+	"toPorts": [{
+	    "ports": [{"port": "80", "protocol": "tcp"}],
+	    "rules": {
+                "HTTP": [{
+		    "method": "GET",
+		    "path": "/public"
+		},{
+		    "method": "GET",
+		    "path": "/headertest",
+		    "headers": ["X-Test: true"]
+                }]
+	    }
 	}]
-}
+    }]
+}]
 EOF
 
 sleep 2
@@ -68,4 +79,10 @@ if [[ "${RETURN//$'\n'}" != "403" ]]; then
 	abort "GET /private, unexpected return"
 fi
 
-cilium policy delete root
+
+RETURN=$(docker exec -i client bash -c "curl -s --output /dev/stderr -w '%{http_code}' --connect-timeout 10 -XGET -H 'X-Test: true' http://$SERVER_IP4:80/headertest")
+if [[ "${RETURN//$'\n'}" != "404" ]]; then
+	abort "GET /headertest, unexpected return"
+fi
+
+cilium policy delete --all
